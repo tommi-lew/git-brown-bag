9f398771fda1f60cb62b28034f790602f3be3605
Tommi <99382026+tommi-lew-tw@users.noreply.github.com>
2023-03-15 13:18:28 +0800

    feat: sing chorus of “I Want It That Way”

--- i_want_it_that_way.rb
+++ i_want_it_that_way.rb
@@ -7,3 +7,13 @@ puts "But we are two worlds apart"
 puts "Can't reach to your heart"
 puts "When you say"
 puts "That I want it that way"
+
+puts "
+Tell me why
+Ain't nothin' but a toothache
+Tell me why
+Ain't nothin' but a mistake
+Tell me why
+I never wanna hear you say
+I want it that way
+"
