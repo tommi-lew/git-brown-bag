63372a8e4d11284559311a09cd62b9d94f07b482
Tommi <99382026+tommi-lew-tw@users.noreply.github.com>
2023-03-16 12:27:12 +0800

    feat: add remaining lyrics of I Want It That Way to complete the song
    - Also rename `backstreet_boys_lyrics` to `lyrics`

--- i_want_it_that_way.rb
+++ i_want_it_that_way.rb
@@ -5,7 +5,7 @@ def print_lyrics_with_delay(lyrics, delay = 0.5)
   end
 end
 
-backstreet_boys_lyrics = <<-LYRICS
+lyrics = <<-LYRICS
 You are my fire
 The one desire
 Believe when I say
@@ -23,6 +23,45 @@ Ain't nothin' but a mistake
 Tell me why
 I never wanna hear you say
 I want it that way
+
+Am I your fire?
+Your one desire
+Yes I know it's too late
+But I want it that way
+
+Tell me why
+Ain't nothin' but a toothache
+Tell me why
+Ain't nothin' but a mistake
+Tell me why
+I never wanna hear you say
+I want it that way
+
+Now I can see that we've fallen apart
+From the way that it used to be, yeah
+No matter the distance
+I want you to know
+That deep down inside of me...
+
+You are my fire
+The one desire
+You are (you are, you are, you are)
+
+Don't wanna hear you say
+Ain't nothin' but a toothache
+Ain't nothin' but a mistake
+(Don't wanna hear you say)
+I never wanna hear you say
+I want it that way
+
+Tell me why
+Ain't nothin' but a toothache
+Tell me why
+Ain't nothin' but a mistake
+Tell me why
+I never wanna hear you say
+(Now number five)
+I want it that way
 LYRICS
 
-print_lyrics_with_delay(backstreet_boys_lyrics)
+print_lyrics_with_delay(lyrics)
